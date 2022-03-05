8f28c1055fb2e6a6c7b279dade6e3d4cd1d6d65d
brain.kim(김경희)/kep <brain.kim@kakaoenterprise.com>
2022-03-06 00:37:48 +0900

    [ADD] First post (compiled)

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Start",
+        "excerpt":"Front Matter   date: 2022-03-06 title: “Github Blog 생성” categories: blog tags: general  목차  toc: true  toc_sticky: true  — from null to zero  ","categories": [],
+        "tags": [],
+        "url": "/start/",
+        "teaser": null
+      }]
